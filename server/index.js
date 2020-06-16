5d2debb98bb1cda5844f00e5aa6f74ee0f1d2615
bettergrammer <wiesnerjjason@gmail.com>
2020-06-16 15:11:12 -0700

    Change port to 80

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 80;
 const bodyParser = require('body-parser');
 const fullPath = '/home/ubuntu/jason-sdc-proxy/client/public/index.html';
 const path = require('path');

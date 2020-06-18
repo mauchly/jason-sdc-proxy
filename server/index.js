5796a78335c6dc63720677873cba2d8cac22315d
bettergrammer <wiesnerjjason@gmail.com>
2020-06-18 13:32:21 -0700

    Update

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const port = 3000;
 const bodyParser = require('body-parser');
 const fullPath = '/home/ubuntu/jason-sdc-proxy/client/public/index.html';
 const path = require('path');
-const loaderioFile = require('../loaderio-4deae93abd54e337b154b38197381d25.txt');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,10 +14,6 @@ app.use(function(req, res, next) {
 
 app.use(express.static(__dirname + '/../public'));
 app.use('/:id', express.static(__dirname + '/../public/index.html'));
-
-app.get('/loaderio-4deae93abd54e337b154b38197381d25', (req, res) => {
-  res.send(loaderioFile);
-});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
